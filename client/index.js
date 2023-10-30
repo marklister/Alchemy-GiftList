654509eba4be0a506f7606d20709861248cdd6d8
mark lister <bushtic2000-general@yahoo.com>
2023-10-30 14:28:36 +0000

    Update client index.js

--- client/index.js
+++ client/index.js
@@ -3,12 +3,18 @@ const niceList = require('../utils/niceList.json');
 const MerkleTree = require('../utils/MerkleTree');
 
 const serverUrl = 'http://localhost:1225';
-
+const merkleTree = new MerkleTree(niceList);
+const root = merkleTree.getRoot();
+const name = 'Norman Block';
+const index = niceList.findIndex(n => n === name);
+const proof = merkleTree.getProof(index);
+const leaf = merkleTree.getProof
 async function main() {
   // TODO: how do we prove to the server we're on the nice list? 
-
+  
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
+    name:name,
+    proof:proof
   });
 
   console.log({ gift });
